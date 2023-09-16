2d59197a09a8351489afcf22e50af3eecff4082f
rachealmuchiri <rachealloveo6@gmail.com>
2023-09-16 18:05:34 +0300

    added the weather icon component

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,5 +1,28 @@
 import React from "react";
-
+import WeatherIcon from "./WeatherIcon";
+import WeatherIcon from "./WeatherIcon";
 export default function WeatherForecast() {
-  return "This is the forecast";
+  return (
+    <div className="WeatherForecast">
+      <div class="row">
+        <div class="col">
+          <div class="weather-forecast-date">
+            ${formatDay(forecastDay.time)}
+            <br />
+            <WeatherIcon />
+            <img src="" class="forecast-image" />
+            <br />
+            <div class="temperature-container">
+              <span class="temperature temperature-1">
+                ${Math.round(forecastDay.temperature.maximum)}°
+              </span>
+              <span class="temperature temperature-2">
+                ${Math.round(forecastDay.temperature.minimum)}°
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
