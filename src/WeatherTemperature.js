6283d1278385b64d481ba70c9827cd6cd8f9d70c
rachealmuchiri <rachealloveo6@gmail.com>
2023-09-16 15:20:53 +0300

    added the temp conversion function

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,18 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
-  return (
-    <span className="weathertemp">
-      <span id="celsius">{Math.round(props.info.temperature)}</span>
-      <span className="degree">
-        <a href="#" id="celcius-link" className="active">
-          ⁰C |{" "}
-        </a>
-        <a href="#" id="faren-link">
-          {" "}
-          ⁰F
-        </a>
-      </span>{" "}
-    </span>
-  );
+  const [unit, setUnit] = useState("celsius");
+
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+
+  function showCelsius(event) {
+    event.preventDefault();
+  }
+
+  if (unit === "celsius") {
+    return (
+      <span className="weathertemp">
+        <span id="celsius">{Math.round(props.info.temperature)}</span>
+        <span className="degree">
+          <a
+            href="#"
+            id="celcius-link"
+            className="active"
+            onClick={showFahrenheit}
+          >
+            ⁰C |{" "}
+          </a>
+          <a href="#" id="faren-link">
+            {" "}
+            ⁰F
+          </a>
+        </span>{" "}
+      </span>
+    );
+  } else {
+    let fahrenheit = (props.info.temperature * 9) / 5 + 32;
+    return (
+      <span className="weathertemp">
+        <span id="celsius">{Math.round(fahrenheit)}</span>
+        <span className="degree">
+          <a
+            href="#"
+            id="celcius-link"
+            className="active"
+            onClick={showCelsius}
+          >
+            ⁰C |{" "}
+          </a>
+          <a href="#" id="faren-link" onClick={convertToCelsius}>
+            {" "}
+            ⁰F
+          </a>
+        </span>{" "}
+      </span>
+    );
+  }
 }
