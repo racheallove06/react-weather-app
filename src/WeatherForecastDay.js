8bd34b08be19060db26ee314b2094a7a202f6adc
rachealmuchiri <rachealloveo6@gmail.com>
2023-09-17 21:48:11 +0300

    made the weatherIcon display

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -1,19 +1,19 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 export default function WeatherForecastDay(props) {
-  console.log("WeatherForecastDay", props.condition.icon);
+  console.log("WeatherForecastDay", props.data.condition.icon);
   return (
     <div class="weather-forecast-date">
       {props.data.time}
       <br />
-      <WeatherIcon code={props.condition.icon} size={36} />
+      <WeatherIcon code={props.data.condition.icon} size={40} />
       <br />
       <div class="temperature-container">
         <span class="temperature temperature-1">
-          {Math.round(props.temperature.maximum)}°
+          {Math.round(props.data.temperature.maximum)}°
         </span>
         <span class="temperature temperature-2">
-          {Math.round(props.temperature.minimum)}°
+          {Math.round(props.data.temperature.minimum)}°
         </span>
       </div>
     </div>
